d102504302d4d485090ef78a9e713d76d15134be
mrdrivingduck <mrdrivingduck@gmail.com>
2022-06-03 21:46:01 +0800

    feat: auto reply when PR opens

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ module.exports = (app) => {
     );
   });
 
+  app.on("pull_request.opened", async (context) => {
+    const user = context.payload.pull_request.user.login;
+    const response = await context.octokit.issues.listForRepo(
+      context.repo({
+        state: "all",
+        creator: user,
+      })
+    );
+    const countPR = response.data.filter((data) => data.pull_request);
+
+    let reply = "Hi @" + user + " ~ ";
+    if (countPR.length === 1) {
+      reply += "Congratulations to your first PR to PolarDB. 🎉";
+      reply += "\n\n";
+    } else {
+      reply += "Thanks for your contribution in this PR. ❤️";
+      reply += "\n\n";
+    }
+
+    reply +=
+      "Please make sure that your PR conforms the standard, and has passed all the checks.";
+    reply += "\n\n";
+    reply += "We will review your PR as soon as possible.";
+
+    return context.octokit.issues.createComment(context.issue({ body: reply }));
+  });
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
