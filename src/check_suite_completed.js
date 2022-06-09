98346090b945883bbaa7b71aa237390e8559f26b
mrdrivingduck <mrdrivingduck@gmail.com>
2022-06-10 00:26:17 +0800

    refactor: infer PR number from head SHA

--- src/check_suite_completed.js
+++ src/check_suite_completed.js
@@ -1,25 +1,3 @@
-const axios = require("axios").default;
-
-/**
- * Get the PR number through Cirrus CI API.
- *
- * @see https://cirrus-ci.org/api/
- * @param {String} external_id
- * @returns The PR number on Cirrus CI, or -1 if not triggered by a PR.
- */
-async function get_pull_request_number_cirrus(external_id) {
-  const _cirrus = await axios.post("https://api.cirrus-ci.com/graphql", {
-    query: "query{task(id: " + external_id + "){build{pullRequest}}}",
-  });
-
-  const result = _cirrus.data.data.task.build.pullRequest;
-
-  if (result != null) {
-    return result;
-  }
-  return -1;
-}
-
 /**
  * Replace the CI label of the pull request.
  *
@@ -67,11 +45,12 @@ module.exports = async (context) => {
   const { check_suite } = context.payload;
   const check_suite_id = check_suite.id;
   const user = check_suite.head_commit.author.name;
-  const conclusion = check_suite.conclusion;
+  const { conclusion, head_sha } = check_suite;
 
   const owner = context.payload.repository.owner.login;
   const repo = context.payload.repository.name;
 
+  /* get the detail of check runs */
   const _check_runs = await context.octokit.checks.listForSuite({
     owner,
     repo,
@@ -79,111 +58,112 @@ module.exports = async (context) => {
   });
   const { check_runs } = _check_runs.data;
 
-  let pull_request_number = -1;
-  for (const check_run of check_runs) {
-    const { external_id } = check_run;
-    const provider = check_run.app.slug;
-
-    if (external_id != null) {
-      if (provider === "cirrus-ci") {
-        pull_request_number = await get_pull_request_number_cirrus(external_id);
-      } else {
-        continue;
-      }
-
-      if (pull_request_number != -1) {
-        break;
-      }
-    }
-  }
-
-  if (pull_request_number === -1) {
-    return;
-  }
-
-  const _labels = await context.octokit.issues.listLabelsOnIssue({
-    owner,
-    repo,
-    issue_number: pull_request_number,
+  /* get the related pull request */
+  const _linked_prs = await context.octokit.search.issuesAndPullRequests({
+    q: head_sha + " SHA",
   });
-  const labels = _labels.data;
+  const linked_prs = _linked_prs.data.items;
 
-  let body = "Hey @" + user + " :\n\n";
-  if (conclusion === "success") {
-    body +=
-      "Congratulations~ 🎉 Your commit has passed all the checks. Please wait for further manual review.";
-    await context.octokit.issues.createComment({
-      owner,
-      repo,
-      issue_number: pull_request_number,
-      body,
-    });
-    return replace_ci_label(
-      context,
-      labels,
-      conclusion,
-      owner,
-      repo,
-      pull_request_number
-    );
-  } else if (conclusion === "cancelled") {
-    body +=
-      "Your checks have been cancelled. Please re-run the checks if you want to merge this PR.";
-    await context.octokit.issues.createComment({
-      owner,
-      repo,
-      issue_number: pull_request_number,
-      body,
-    });
-    return replace_ci_label(
-      context,
-      labels,
-      conclusion,
-      owner,
-      repo,
-      pull_request_number
-    );
-  } else {
-    body +=
-      "Something wrong occuried during the checks of your commit, please check the detail: 🤓\n\n";
-  }
+  for (const linked_pr of linked_prs) {
+    const { state, html_url, labels } = linked_pr;
 
-  check_runs.forEach((check_run) => {
+    /* PR is not open, skip it */
+    if (state !== "open") {
+      continue;
+    }
+
+    /* Not current repository, skip it */
+    const url_tokens = html_url.split("/");
+    const url_token_len = url_tokens.length;
     if (
-      check_run.conclusion !== "success" &&
-      check_run.conclusion !== "skipped"
+      url_token_len < 4 ||
+      url_tokens[url_token_len - 2] !== "pull" ||
+      url_tokens[url_token_len - 3] !== repo ||
+      url_tokens[url_token_len - 4] !== owner
     ) {
-      const check_run_name = check_run.name;
-      const check_run_url = check_run.details_url;
-      const check_run_text = check_run.output.text;
-
-      body += "<details>\n";
-      body +=
-        "<summary> ⚠️ " +
-        check_run_name +
-        " <a href='" +
-        check_run_url +
-        "'>View more details</a>" +
-        "</summary>\n\n";
-      body += check_run_text;
-      body += "\n\n";
-      body += "</details>\n\n";
+      continue;
     }
-  });
 
-  await context.octokit.issues.createComment({
-    owner,
-    repo,
-    issue_number: pull_request_number,
-    body,
-  });
+    const pull_request_number = parseInt(url_tokens[url_token_len - 1]);
 
-  return replace_ci_label(
-    context,
-    labels,
-    conclusion,
-    owner,
-    repo,
-    pull_request_number
-  );
+    let body = "Hey @" + user + " :\n\n";
+    if (conclusion === "success") {
+      body +=
+        "Congratulations~ 🎉 Your commit has passed all the checks. Please wait for further manual review.";
+      await context.octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: pull_request_number,
+        body,
+      });
+      return replace_ci_label(
+        context,
+        labels,
+        conclusion,
+        owner,
+        repo,
+        pull_request_number
+      );
+    } else if (conclusion === "cancelled") {
+      body +=
+        "Your checks have been cancelled. Please re-run the checks if you want to merge this PR.";
+      await context.octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: pull_request_number,
+        body,
+      });
+      return replace_ci_label(
+        context,
+        labels,
+        conclusion,
+        owner,
+        repo,
+        pull_request_number
+      );
+    } else {
+      body +=
+        "Something wrong occuried during the checks of your commit, please check the detail: 🤓\n\n";
+    }
+
+    /* Detail information of unsuccessful check runs */
+    check_runs.forEach((check_run) => {
+      if (
+        check_run.conclusion !== "success" &&
+        check_run.conclusion !== "skipped"
+      ) {
+        const check_run_name = check_run.name;
+        const check_run_url = check_run.details_url;
+        const check_run_text = check_run.output.text;
+
+        body += "<details>\n";
+        body +=
+          "<summary> ⚠️ " +
+          check_run_name +
+          " <a href='" +
+          check_run_url +
+          "'>View more details</a>" +
+          "</summary>\n\n";
+        body += check_run_text;
+        body += "\n\n";
+        body += "</details>\n\n";
+      }
+    });
+
+    await context.octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: pull_request_number,
+      body,
+    });
+
+    return await replace_ci_label(
+      context,
+      labels,
+      conclusion,
+      owner,
+      repo,
+      pull_request_number
+    );
+  }
 };
